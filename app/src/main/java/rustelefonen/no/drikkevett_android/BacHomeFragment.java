a86a60c91a58bf3d20743828a48006ccd85d851b
Simen Fonnes <fonsim13@student.westerdals.no>
2016-07-04 15:55:42 +0200

    refac

--- app/src/main/java/rustelefonen/no/drikkevett_android/BacHomeFragment.java
+++ app/src/main/java/rustelefonen/no/drikkevett_android/BacHomeFragment.java
@@ -14,7 +14,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.ChartData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -46,6 +53,8 @@ public class BacHomeFragment extends Fragment{
     public TextView lastMonthHighestBacTextView;
     public TextView lastMonthAvgBacTextView;
 
+    public PieChart goalPieChart;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bac_home_frag, container, false);
@@ -53,18 +62,49 @@ public class BacHomeFragment extends Fragment{
         initWidgets(view);
         fillWidgets();
 
-        List<History> historyList = getHistoryList();
 
-        if (historyList.size() > 0) {
-            totalCostTextView.setText(getTotalCost(historyList) + ",-\nKostnader");
-            totalHighestBac.setText(getTotalHighestBac(historyList) + "\nHøyeste\nPromille");
-            totalAvgTextView.setText(getTotalAverageHighestBac(historyList) + "\nGjennomsnitt\nhøyeste promille");
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
 
-            lastMonthCostTextView.setText(getLastMonthCost(historyList) + ",-\nKostnader");
-            lastMonthHighestBacTextView.setText(getLastMonthHighestBac(historyList) + "\nHøyeste\npromille");
-            lastMonthAvgBacTextView.setText(getLastMonthAverageBac(historyList) + "\nGjennomsnitt\nhøyeste promille");
-        }
+        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
 
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        PieData data = new PieData(labels, dataset); // initialize Piedata
+        goalPieChart.setData(data);
+
+
+
+
+        //Styling chart:
+
+        //pieChartDataSet.colors = [UIColor(red: 193/255.0, green: 26/255.0, blue: 26/255.0, alpha: 1.0), UIColor(red:26/255.0, green: 193/255.0, blue: 73/255.0, alpha: 1.3)]
+
+        goalPieChart.setDrawHoleEnabled(true);
+        goalPieChart.setHoleRadius((float) 0.80);
+        goalPieChart.setHoleColor(1);   //Usikker UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 0.0)
+        goalPieChart.setCenterTextRadiusPercent((float) 1.0);
+        goalPieChart.setTransparentCircleRadius((float) 0.85);
+        //goalPieChart.setAnimation();
+        goalPieChart.setDescription("");
+        goalPieChart.setBackgroundColor(1); //Usikker UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 0.0)
+        goalPieChart.setTransparentCircleColor(1); //Usikker
+        goalPieChart.setDrawSliceText(false);
+        goalPieChart.getLegend().setEnabled(false);
+        //pieChartView.userInteractionEnabled = false
+
+        goalPieChart.setCenterText("0.5");
 
         return view;
     }
@@ -165,6 +205,8 @@ public class BacHomeFragment extends Fragment{
         lastMonthCostTextView = (TextView) view.findViewById(R.id.last_month_cost_text_view);
         lastMonthHighestBacTextView = (TextView) view.findViewById(R.id.last_month_highest_bac_text_view);
         lastMonthAvgBacTextView = (TextView) view.findViewById(R.id.last_month_avg_bac_text_view);
+
+        goalPieChart = (PieChart) view.findViewById(R.id.goal_pie_chart);
     }
 
     private void fillWidgets() {
@@ -172,6 +214,18 @@ public class BacHomeFragment extends Fragment{
         helloMessageTextView.setText(getRandomWelcomeMessage());
         quoteTextView.setText(getRandomQuote());
         insertImageIfExists();
+
+        List<History> historyList = getHistoryList();
+
+        if (historyList.size() > 0) {
+            totalCostTextView.setText(getTotalCost(historyList) + ",-\nKostnader");
+            totalHighestBac.setText(getTotalHighestBac(historyList) + "\nHøyeste\nPromille");
+            totalAvgTextView.setText(getTotalAverageHighestBac(historyList) + "\nGjennomsnitt\nhøyeste promille");
+
+            lastMonthCostTextView.setText(getLastMonthCost(historyList) + ",-\nKostnader");
+            lastMonthHighestBacTextView.setText(getLastMonthHighestBac(historyList) + "\nHøyeste\npromille");
+            lastMonthAvgBacTextView.setText(getLastMonthAverageBac(historyList) + "\nGjennomsnitt\nhøyeste promille");
+        }
     }
 
     private void insertUser() {
