7d39cfeeef1ff7336b9d0a562708c4238ad14c61
Simen Fonnes <fonsim13@student.westerdals.no>
2016-07-06 00:19:10 +0200

    barchart is dynamic and has most of the style

--- app/src/main/java/rustelefonen/no/drikkevett_android/BacHomeFragment.java
+++ app/src/main/java/rustelefonen/no/drikkevett_android/BacHomeFragment.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -31,6 +32,7 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.File;
+import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -78,11 +80,36 @@ public class BacHomeFragment extends Fragment{
 
         BarData data = new BarData(getXAxisValues(), getDataSet());
         historyBarChart.setData(data);
-        historyBarChart.setDescription("My Chart");
+        /*historyBarChart.setDescription("My Chart");
         historyBarChart.animateXY(2000, 2000);
-        historyBarChart.invalidate();
+        historyBarChart.invalidate();*/
+
+        historyBarChart.setNoDataText("ingen graf.");
+        historyBarChart.getAxisLeft().setDrawGridLines(false);
+        historyBarChart.getAxisRight().setDrawGridLines(false);
+        historyBarChart.getXAxis().setDrawGridLines(false);
+        //self.lineGraph.xAxis.labelPosition = .Bottom
+        historyBarChart.getAxisRight().setDrawTopYLabelEntry(false);
+        //self.lineGraph.leftAxis.labelTextColor = UIColor.whiteColor()
+        historyBarChart.getXAxis().setTextColor(Color.parseColor("#FFFFFF"));
+        //self.lineGraph.rightAxis.enabled = false
+        historyBarChart.setBackgroundColor(Color.parseColor("#141414"));
+        historyBarChart.getLegend().setEnabled(false);
+        //self.lineGraph.descriptionTextColor = UIColor.redColor()
+        //self.lineGraph.userInteractionEnabled = false
+        historyBarChart.setPinchZoom(false);
+        historyBarChart.setDoubleTapToZoomEnabled(false);
 
 
+        historyBarChart.getAxisRight().removeAllLimitLines();
+        historyBarChart.animateXY(1000, 1000);
+
+        User user = getUser();
+
+        LimitLine limit = new LimitLine(user.getGoalBAC().floatValue(), "");
+        limit.setLineColor(Color.parseColor("#FFFFFF"));
+        //ll.lineDashLengths = [8.5]
+        historyBarChart.getAxisRight().addLimitLine(limit);
 
         return view;
     }
@@ -91,37 +118,27 @@ public class BacHomeFragment extends Fragment{
         ArrayList<IBarDataSet> dataSets = null;
 
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-        valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
-        valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
-        valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
-        valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-        valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
-        valueSet1.add(v1e6);
-
         ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
+
+        List<History> historyList = getHistoryList();
+
+        User user = getUser();
+        double goal = user.getGoalBAC();
+
+        int counter = 0;
+        for (History history : historyList) {
+            if (history.getHighestBAC() > goal) {
+                valueSet2.add(new BarEntry(history.getHighestBAC().floatValue(), counter));
+            } else {
+                valueSet1.add(new BarEntry(history.getHighestBAC().floatValue(), counter));
+            }
+            counter++;
+        }
 
         BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
         barDataSet1.setColor(Color.rgb(0, 155, 0));
         BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet2.setColor(Color.RED);
 
         dataSets = new ArrayList<>();
         dataSets.add(barDataSet1);
@@ -131,14 +148,49 @@ public class BacHomeFragment extends Fragment{
 
     private ArrayList<String> getXAxisValues() {
         ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
+
+        List<History> historyList = getHistoryList();
+        for (History history : historyList) {
+
+            Calendar cal = Calendar.getInstance();
+            Date date = history.getStartDate();
+            cal.setTime(date);
+
+            xAxis.add(cal.get(Calendar.DAY_OF_MONTH) + " " + getMonthName(cal.get(Calendar.MONTH)));
+        }
         return xAxis;
     }
+
+    private String getMonthName(int month) {
+        switch (month) {
+            case 0:
+                return "Januar";
+            case 1:
+                return "Februar";
+            case 2:
+                return "Mars";
+            case 3:
+                return "April";
+            case 4:
+                return "Mai";
+            case 5:
+                return "Juni";
+            case 6:
+                return "Juli";
+            case 7:
+                return "August";
+            case 8:
+                return "September";
+            case 9:
+                return "Oktober";
+            case 10:
+                return "November";
+            case 11:
+                return "Desember";
+        }
+        return "";
+    }
+
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
@@ -462,14 +514,8 @@ public class BacHomeFragment extends Fragment{
     }
 
     private void stylePieChart() {
-        //style
-
         User user = getUser();
-        if (user != null) {
-            goalPieChart.setCenterText(user.getGoalBAC() +"");
-        }
-
-        //goalPieChart.setCenterText("0.5");
+        if (user != null) goalPieChart.setCenterText(user.getGoalBAC() +"");
         goalPieChart.setDrawHoleEnabled(true);
         goalPieChart.setHoleRadius(80f);
         goalPieChart.setHoleColor(Color.parseColor("#141414"));
@@ -479,15 +525,10 @@ public class BacHomeFragment extends Fragment{
         goalPieChart.setBackgroundColor(Color.parseColor("#141414"));
         goalPieChart.setAnimation(new Animation() {
         });
-        //goalPieChart.setTransparentCircleColor(Color.tr);
         goalPieChart.setDrawSliceText(false);
         goalPieChart.getLegend().setEnabled(false);
         goalPieChart.setRotationEnabled(false);
-
         goalPieChart.setDrawSliceText(false);
-        //goalPieChart.setDrawValues(false);
-
-
         goalPieChart.setCenterTextSize(27.0f);
         goalPieChart.setCenterTextColor(Color.parseColor("#FFFFFF"));
         goalPieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
