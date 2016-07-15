590dca9b1a562a5a54dbc2f7207f1679ee25b016
Simen Fonnes <fonsim13@student.westerdals.no>
2016-07-15 10:26:00 +0200

    made it impossible to go back to registration when user has been created.

--- app/src/main/java/rustelefonen/no/drikkevett_android/MainActivity.java
+++ app/src/main/java/rustelefonen/no/drikkevett_android/MainActivity.java
@@ -23,6 +23,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -375,6 +376,19 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
             return "";
         }
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5 && keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onBackPressed() {}
 
 
 }
---
+++ app/src/main/java/rustelefonen/no/drikkevett_android/settings/AlcoholPricingSettingsActivity.java
@@ -0,0 +1,10 @@
+package rustelefonen.no.drikkevett_android.settings;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by simenfonnes on 15.07.2016.
+ */
+
+public class AlcoholPricingSettingsActivity extends AppCompatActivity {
+}
