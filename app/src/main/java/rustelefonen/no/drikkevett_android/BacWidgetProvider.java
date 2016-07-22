cc7cbd0366ee0dd8318119db1def3c57bca33bf0
Simen Fonnes <fonsim13@student.westerdals.no>
2016-07-22 10:31:11 +0200

    added radiogroup for viewpager

--- app/src/main/java/rustelefonen/no/drikkevett_android/BacWidgetProvider.java
+++ app/src/main/java/rustelefonen/no/drikkevett_android/BacWidgetProvider.java
@@ -7,13 +7,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import rustelefonen.no.drikkevett_android.db.DayAfterBAC;
+import rustelefonen.no.drikkevett_android.db.DayAfterBACDao;
 import rustelefonen.no.drikkevett_android.db.PlanPartyElements;
 import rustelefonen.no.drikkevett_android.db.PlanPartyElementsDao;
 import rustelefonen.no.drikkevett_android.tabs.dayAfter.BacDayAfterFragment;
 import rustelefonen.no.drikkevett_android.tabs.home.SuperDao;
 import rustelefonen.no.drikkevett_android.tabs.planParty.BacPlanPartyFragment;
+import rustelefonen.no.drikkevett_android.util.PartyUtil;
+
+import static rustelefonen.no.drikkevett_android.util.PartyUtil.calculateBAC;
+import static rustelefonen.no.drikkevett_android.util.PartyUtil.countingGrams;
+import static rustelefonen.no.drikkevett_android.util.PartyUtil.getDateDiff;
+import static rustelefonen.no.drikkevett_android.util.PartyUtil.setGenderScore;
 
 /**
  * Created by simenfonnes on 21.07.2016.
@@ -57,4 +68,5 @@ public class BacWidgetProvider extends AppWidgetProvider {
     private double getCurrentBac(Context context) {
         return 0.0;
     }
+
 }
