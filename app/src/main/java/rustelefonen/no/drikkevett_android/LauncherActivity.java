8f3a5e65e7ba9fe6d0e63f951075bc2de8beed7e
Simen Fonnes <fonsim13@student.westerdals.no>
2016-08-01 20:57:40 +0200

    added splash screen

--- app/src/main/java/rustelefonen/no/drikkevett_android/LauncherActivity.java
+++ app/src/main/java/rustelefonen/no/drikkevett_android/LauncherActivity.java
@@ -22,47 +22,23 @@ import rustelefonen.no.drikkevett_android.tabs.home.SuperDao;
 
 public class LauncherActivity extends AppCompatActivity {
 
-    private User user;
-
     private boolean hasUser() {
         SuperDao superDao = new SuperDao(this);
         UserDao userDao = superDao.getUserDao();
-
         List<User> users = userDao.queryBuilder().list();
         superDao.close();
-
         if (users.size() <= 0) return false;
-        user = users.get(0);
-
+        User user = users.get(0);
         return user != null;
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*if (!hasInfoInDB()) {
-            System.out.println("har ikke mer enn 0");
-            DBSeeder.seed(this);
-        } else {
-            System.out.println("har mer enn 0");
-        }*/
         Intent intent;
-        if (hasUser()) {
-            intent = new Intent(this, MainActivity.class);
-            intent.putExtra(MainActivity.ID, user);
-        } else {
-            intent = new Intent(this, WelcomeActivity.class);
-        }
+        if (hasUser()) intent = new Intent(this, MainActivity.class);
+        else intent = new Intent(this, WelcomeActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    private boolean hasInfoInDB() {
-        SuperDao superDao = new SuperDao(this);
-        InformationCategoryDao informationCategoryDao = superDao.getInformationCategoryDao();
-        int size = informationCategoryDao.queryBuilder().list().size();
-        superDao.close();
-        return size > 0;
     }
 }
